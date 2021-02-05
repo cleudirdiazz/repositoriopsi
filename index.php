a78be073043033d555cd4683de8d520728f938b6
Cleudir Diaz <78570756+cleudirdiazz@users.noreply.github.com>
2021-02-04 21:01:11 -0300

    Inicio da aplicação php no repositorio index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+    <!DOCTYPE html>
+    <html lang="pt-br">
+      <head>
+        <title>Repositório PSI</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1>
+          <?php   
+            echo "Esse é o repositório de uma das melhores materias desse curso!";
+          ?>
+        </h1>
+      </body>
+    </html>
